a0d62ce48de25e054226838a08ea61edfa63a858
juliagg14 <137561082+juliagg14@users.noreply.github.com>
2024-05-09 00:25:20 -0300

    .

---
+++ app/src/main/java/com/example/bancodip/view/CadastroActivity.java
@@ -0,0 +1,69 @@
+package com.example.bancodip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.bancodip.controller.ControllerBancoDados;
+import com.example.bancodip.controller.Util;
+import com.example.bancodip.databinding.ActivityCadastroBinding;
+import com.example.bancodip.databinding.ActivityRegisterBinding;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private ActivityCadastroBinding binding;
+    private ControllerBancoDados controllerBancoDados;
+    private Util util;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCadastroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        controllerBancoDados = new ControllerBancoDados(this);
+        util = new Util();
+        Intent intent = new Intent(CadastroActivity.this, MainActivity.class);
+
+        binding.btnConfirmarCadastro.setOnClickListener(v -> {
+            controllerBancoDados.open();
+
+            String  nome = binding.inputNome.getEditText().toString().toUpperCase().trim();
+            String email = binding.inputEmail.getEditText().toString().toUpperCase().trim();
+            String saldo = binding.inputSaldo.getEditText().toString().trim();
+
+            if(!nome.isEmpty() && !email.isEmpty() && !saldo.isEmpty() && util.isValidEmail(email) && !controllerBancoDados.isEmailInDatabase(email) ){
+
+                double saldoDouble = Double.parseDouble(saldo);
+                double chequeEspecial = saldoDouble * 4;
+
+                try {
+                    controllerBancoDados.insertData(nome, email, saldoDouble, chequeEspecial, chequeEspecial);
+                    intent.putExtra("nome", nome);
+                    intent.putExtra("email", email);
+                    intent.putExtra("saldo", saldoDouble);
+                    intent.putExtra("cheque", chequeEspecial);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                } finally {
+                    controllerBancoDados.close();
+                    startActivity(intent);
+                    finish();
+                }
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Conta já existente", Toast.LENGTH_LONG).show();
+            }
+
+
+
+        });
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/bancodip/view/LoginActivity.java
@@ -0,0 +1,65 @@
+package com.example.bancodip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.bancodip.controller.ControllerBancoDados;
+import com.example.bancodip.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private ControllerBancoDados controllerBancoDados;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intentRegister = new Intent(LoginActivity.this, CadastroActivity.class);
+        Intent intentMain = new Intent(LoginActivity.this, MainActivity.class);
+
+        controllerBancoDados = new ControllerBancoDados(this);
+
+        binding.btnCadastrar.setOnClickListener(v -> {
+            startActivity(intentRegister);
+        });
+
+        binding.btnLogar.setOnClickListener(v -> {
+            controllerBancoDados.open();
+
+            String nome = binding.loginNome.getEditText().toString().trim().toUpperCase();
+            String email = binding.loginEmail.getEditText().toString().trim().toUpperCase();
+
+
+            if (controllerBancoDados.isNomeInDatabase(nome) && controllerBancoDados.isEmailInDatabase(email)){
+
+                try {
+                    intentMain.putExtra("nome", nome);
+                    intentMain.putExtra("email", email);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    controllerBancoDados.close();
+                    startActivity(intentMain);
+                    finish();
+                }
+
+
+
+            }else {
+                Toast.makeText(getApplicationContext(), "Não foi possivel se conectar", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+
+
+    }
+}
